29bd28a560dc0b809da3b6b0c04319c2136cf668
gmargali <gal.margalit@alcatel-lucent.com>
2015-06-15 16:15:31 +0300

    revision 2

--- app-compiled.js
+++ app-compiled.js
@@ -1,7 +1,15 @@
 "use strict";
 
-/**
- * Created by gmargali on 11/6/2015.
- */
+SC.initialize({
+    client_id: "19d63840c9dcfcd6801ed142b3aaf3ca",
+    redirect_uri: "https://github.com/Wavez/Soundcloud-Stats/blob/master/callback.html"
+});
+
+// initiate auth popup
+SC.connect(function () {
+    SC.get("/me", function (me) {
+        alert("Hello, " + me.username);
+    });
+});
 
 //# sourceMappingURL=app-compiled.js.map
